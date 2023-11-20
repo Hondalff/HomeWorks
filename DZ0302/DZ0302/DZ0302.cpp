f5d9fbe8e79eb6a54967840ab9064aec440eb6a1
Dmitriy Baturinets <dmitriydiablo@mail.ru>
2023-11-20 18:46:30 +0300

    bug fixing

--- DZ0302/DZ0302/DZ0302.cpp
+++ DZ0302/DZ0302/DZ0302.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <Windows.h>
 
 
 class Counter
@@ -6,55 +7,51 @@ class Counter
 private:
     int count = 0;
 public:
-    int get_count() { return count; }
-    int set_count(int value) { count = value; return count; }
-
-    int Add() { return count++; }
+    
+    int get_count() { count; }    
+    int Add() {return this ->count++; }
     int Divide() { return count--; }
-    void Info() { std::cout << count; }
-    bool Exit() { return true; }
+    void Info() { std::cout << count << std::endl; }
+    bool Exit() { true; }
+
+    Counter(int count)
+    {
+        this->count = count;
+    }
 };
 
-void StateProg(bool& close)
-{
-    char text;
-    int num1;
-    Counter count;
-
-    std::cout << "Добро пожаловать! " << std::endl;
-    std::cout << "Вы хотите указать начальное значение счётчика? Введите да или нет: " ;
-    std::cin >> text;
-
-    if (text == 'да')
-    {
-        std::cout << "Введите начальное значение счётчика: ";
-        std::cin >> num1;
-        count.set_count(num1);
-    }
-    
-    std::cout << "\nВведите команду ('+', '-', '=' или 'x'): ";
-    std::cin >> text;
-
-    if(text == '+') { count.Add(); }
-    else if (text == '-') { count.Divide(); }
-    else if (text == '=') { count.Info(); }
-    else if (text == 'x') { close = false; }
-
-  
-
-}
 
 int main()
 {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
     setlocale(LC_ALL, "Russian");
     bool close = true;
+    bool useFirst = false;
+    std::string text;
+    int num1 = 0;
     
+    std::cout << "Добро пожаловать! " << std::endl;
+    std::cout << "Вы хотите указать начальное значение счётчика? Введите да или нет: ";
+    std::cin >> text;
+
+    if (text == "да")
+    {
+        std::cout << "Введите начальное значение счётчика: ";
+        std::cin >> num1;
+    }
+
+    Counter count(num1);
 
     while (close)
     {
-        StateProg(close);
-        system("PAUSE");
-        system("cls");
+        std::cout << "\nВведите команду ('+', '-', '=' или 'x'): ";
+        std::cin >> text;
+
+        if (text == "+") { count.Add(); }
+        else if (text == "-") { count.Divide(); }
+        else if (text == "=") { count.Info(); }
+        else if (text == "х") { close = false; }
     }
     return 0;
 }
