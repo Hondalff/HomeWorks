3deb4252c4250470bac85d4c81d6b31ce131593c
Dmitriy Baturinets <dmitriydiablo@mail.ru>
2023-11-25 13:30:54 +0300

    added

--- DZ0402/DZ0402/DZ0402.cpp
+++ DZ0402/DZ0402/DZ0402.cpp
@@ -2,21 +2,27 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <cstring>
 
 class Adress // класс адрес, который хранит в себе данные
 {
 public:
-	
+
 	Adress()
 	{
-		city = street = "None";		
+		city = street = "None";
 		home = apartment = 0;
 	}
-	Adress(std::string _city, std::string _street, int _home, int _apartment) : city(_city), street(_street), home(_home), apartment(_apartment){}
+	Adress(std::string _city, std::string _street, int _home, int _apartment) : city(_city), street(_street), home(_home), apartment(_apartment) {}
 
 	std::string OutputAdress()
 	{
 		return info = city + ", " + street + ", " + std::to_string(home) + ", " + std::to_string(apartment);
+	}
+
+	std::string GetCity()
+	{
+		return city;
 	}
 
 private:
@@ -24,16 +30,29 @@ private:
 	int home, apartment;
 };
 
+void sort(Adress* arr, int SIZE)
+{
+
+	for (int i = 0; i < SIZE; i++)
+	{
+		for (int j = 0; j < SIZE; j++)
+		{
+			
+		}
+	}
+
+}
+
 int main()
 {
-    setlocale(LC_ALL, "Russian");	
+	setlocale(LC_ALL, "Russian");
 
 	int SIZE; // переменная для записи количества адрессов и измерение размера массива
 	std::string city, street;
 	int home, apartment;
 
-	std::ifstream in_file("G:\\Code_Project\\HomeWorks\\DZ0401\\DZ0401\\in.txt"); // открытие вайла in
-	std::ofstream out_file("G:\\Code_Project\\HomeWorks\\DZ0401\\DZ0401\\out.txt"); //открытие файла out
+	std::ifstream in_file("G:\\Code_Project\\HomeWorks\\DZ0402\\DZ0402\\in.txt"); // открытие вайла in
+	std::ofstream out_file("G:\\Code_Project\\HomeWorks\\DZ0402\\DZ0402\\out.txt"); //открытие файла out
 
 	if (in_file.is_open())
 	{
@@ -52,9 +71,11 @@ int main()
 			arr[i] = Adress(city, street, home, apartment);
 		}
 
+		sort(arr, SIZE);
+
 		out_file << SIZE << std::endl;
 
-		for (int j = SIZE-1; j > -1; j--) // выводим адрес в файл в обратном порядке.
+		for (int j = SIZE - 1; j > -1; j--) // выводим адрес 
 		{
 			out_file << arr[j].OutputAdress() << std::endl;
 		}
