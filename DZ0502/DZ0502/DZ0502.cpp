22fa31525bf841b840a5c3c0e5f43d78ab5dc7d0
Dmitriy Baturinets <dmitriydiablo@mail.ru>
2023-12-01 20:35:58 +0300

    DZ0502/03 final

--- DZ0502/DZ0502/DZ0502.cpp
+++ DZ0502/DZ0502/DZ0502.cpp
@@ -3,7 +3,7 @@
 class Figure
 {
 public:
-      
+    virtual void print() = 0;
 protected:
     std::string name;
 };
@@ -19,23 +19,50 @@ public:
     {
         name = "Треугольник";
     }
-   
+    bool check()
+    {
+        int a = A + B + C;
+        if (a == 180)
+        {
+            return true;
+        }
+        return false;
+    }
     virtual void print()
     {
         std::cout << name << ": " << std::endl;
+        if (check())
+        {
+            std::cout << "Правильная" << std::endl;
+        }
+        else
+        {
+            std::cout << "Неправильная" << std::endl;
+        }
         std::cout << "Количество сторон: 3" << std::endl;
         std::cout << "Стороны: " << "a: " << a << " b: " << b << " c: " << c << std::endl;
         std::cout << "углы: " << "A: " << A << " B: " << B << " C: " << C << std::endl;
-      
+        std::cout << std::endl;
     }
 };
 
 class R_triangle : public Triangle
 {
 public:
-    R_triangle(double a, double b, double c, double A, double B, double C) : Triangle(a,b,c,A,B,90)
+    R_triangle(double a, double b, double c, double A, double B) : Triangle(a,b,c,A,B,90)
     {
         name = "прямоугольный треугольник";
+    }
+    bool check()
+    {
+        if (R_triangle::check())
+        {
+            if (this ->C == 90)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 
 };
@@ -47,6 +74,18 @@ public:
     {
         name = "равнобедренный треугольник";
     }
+    bool check()
+    {
+        if (I_triangle::check())
+        {
+            if (a == c && A == C)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
 };
 
 class Eq_triangle : public Triangle
@@ -55,6 +94,17 @@ public:
     Eq_triangle(double a) : Triangle(a, a, a, 60, 60, 60)
     {
         name = "равносторонний треугольник ";
+    }
+    bool check()
+    {
+        if (Eq_triangle::check())
+        {
+            if (a == c == b && A == B == C == 60)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 };
 
@@ -71,10 +121,28 @@ public:
     virtual void print()
     {
         std::cout << name << ": " << std::endl;
-        std::cout << "Количество сторон: 3" << std::endl;
+        if (check())
+        {
+            std::cout << "Правильная" << std::endl;
+        }
+        else
+        {
+            std::cout << "Неправильная" << std::endl;
+        }
+        std::cout << "Количество сторон: 4" << std::endl;
         std::cout << "Стороны: " << "a: " << a << " b: " << b << " c: " << c << " d: " << d << std::endl;
         std::cout << "углы: " << "A: " << A << " B: " << B << " C: " << C << " D: " << D << std::endl;
+        std::cout << std::endl;
 
+    }
+    bool check()
+    {
+        int a = A + B + C + D;
+        if (a == 360)
+        {
+            return true;
+        }
+        return false;
     }
 };
 
@@ -85,6 +153,17 @@ public:
     {
         name = "Ромб";
     }
+    bool check()
+    {
+        if (Quadrangle::check())
+        {
+            if (a == b == c == d && A == C && B == D)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
 };
 
 class Square : public Rhombus
@@ -94,6 +173,17 @@ public:
     {
         name = "Квадрат";
     }
+    bool check()
+    {
+        if (Rhombus::check())
+        {
+            if (a==b==c==d && A==B==C==D==90)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
 };
 
 class Rectangle : public Quadrangle
@@ -102,6 +192,17 @@ public:
     Rectangle(double a, double b) : Quadrangle(a, b, a, b, 90, 90, 90, 90)
     {
         name = "прямоугольник";
+    }
+    bool check()
+    {
+        if (Quadrangle::check())
+        {
+            if (a == c && b == d && A == B == C == D == 90)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 };
 
@@ -113,18 +214,47 @@ public:
     {
         name = "Параллелограмм";
     }
+    bool check()
+    {
+        if (Quadrangle::check())
+        {
+            if (a == b && c == d && A == C && B == D)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
 };
 
+
+void print_info(Figure* f)
+{
+    f->print();
+}
 
 int main()
 {
     setlocale(LC_ALL, "Russian");
     Triangle triangle(10,20,30,50,60,70);
+    R_triangle r_tri(10,20,30,50,60);
+    I_triangle i_triangle(10, 20, 50, 60);
+    Eq_triangle eq_triangle(30);
+    Quadrangle quadrangle(10, 20, 30, 40, 50, 60, 70, 80);
+    Rectangle rectangle(10, 20);
+    Square square(20);
+    Parallelogram parallelogram(20, 30, 30, 40);
+    Rhombus rhombus(30, 30, 40);
 
-    
-   
-    
-   
-  
+    print_info(&triangle);
+    print_info(&r_tri);
+    print_info(&i_triangle);
+    print_info(&eq_triangle);
+    print_info(&quadrangle);
+    print_info(&rectangle);
+    print_info(&square);
+    print_info(&parallelogram);
+    print_info(&rhombus);
+
     return 0;
 }
