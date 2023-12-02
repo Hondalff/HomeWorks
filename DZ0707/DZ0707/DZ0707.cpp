8889ee2bf79607cb6fccbb28b5281a11dcd11551
Dmitriy Baturinets <dmitriydiablo@mail.ru>
2023-12-02 09:05:57 +0300

    Added DZ0701

---
+++ DZ0707/DZ0707/DZ0707.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#define MODE 1
+
+void add()
+{
+    int a, b;
+    std::cout << "Введите число 1: ";
+    std::cin >> a;
+
+    std::cout << "Введите число 2: ";
+    std::cin >> b;
+
+    std::cout << "Результат сложения: " << a + b << std::endl;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+#ifdef MODE
+#if MODE == 0
+    std::cout << "Работаю в режиме тренировки" << std::endl;
+#elif MODE == 1
+    std::cout << "Работаю в боевом режиме" << std::endl;
+    add();
+#else
+    std::cout << "Неизвестный режим. Завершение работы" << std::endl;
+
+#endif // MODE == 0
+#else
+#error "MODE не определена. Определите её!"
+#endif // MODE
+
+
+}
