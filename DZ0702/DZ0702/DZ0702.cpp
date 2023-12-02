df567f6fd4404ca6685a6a0bb31fda4493d72b30
Dmitriy Baturinets <dmitriydiablo@mail.ru>
2023-12-02 09:31:36 +0300

    Added DZ0702

---
+++ DZ0702/DZ0702/DZ0702.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#define SUB(x,y) ((x) - (y))
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+	int a = 6;
+	int b = 5;
+	int c = 2;
+	std::cout << SUB(a, b) << std::endl;
+	std::cout << SUB(a, b) * c << std::endl;
+	std::cout << SUB(a, b + c) * c << std::endl;
+}
