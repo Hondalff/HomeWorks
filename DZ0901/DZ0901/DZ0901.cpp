3239b0133d265c3774dd3a9fd3f26691b884a9b2
Dmitriy Baturinets <dmitriydiablo@mail.ru>
2023-12-09 15:11:00 +0300

    Added DZ0901

---
+++ DZ0901/DZ0901/DZ0901.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+
+class Fraction
+{
+private:
+	int numerator_;
+	int denominator_;
+
+public:
+	Fraction(int numerator, int denominator)
+	{
+		numerator_ = numerator;
+		denominator_ = denominator;
+	}
+	 bool operator==(Fraction right)
+	{
+		 if (numerator_ == right.numerator_ && denominator_ == right.denominator_) { return true; }
+		 else { return false; }
+	}
+	 bool operator!=(Fraction right)
+	 {
+		 if (numerator_ != right.numerator_ || denominator_ != right.denominator_) { return true; }
+		 else { return false; }
+	 }
+	 bool operator<(Fraction right)
+	 {
+		 int a, b;
+		 a = numerator_ + denominator_;
+		 b = right.numerator_ + right.denominator_;
+		 if (a < b) { return true; }
+		 else { return false; }
+	 }
+	 bool operator>(Fraction right)
+	 {
+		 int a, b;
+		 a = numerator_ + denominator_;
+		 b = right.numerator_ + right.denominator_;
+		 if (a > b) { return true; }
+		 else { return false; }
+	 }
+	 bool operator<=(Fraction right)
+	 {
+		 int a, b;
+		 a = numerator_ + denominator_;
+		 b = right.numerator_ + right.denominator_;
+		 if (a <= b) { return true; }
+		 else { return false; }
+	 }
+	 bool operator>=(Fraction right)
+	 {
+		 int a, b;
+		 a = numerator_ + denominator_;
+		 b = right.numerator_ + right.denominator_;
+		 if (a >= b) { return true; }
+		 else { return false; }
+	 }
+};
+
+int main()
+{
+	Fraction f1(4, 3);
+	Fraction f2(3, 11);
+
+	std::cout << "f1" << ((f1 == f2) ? " == " : " not == ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 != f2) ? " != " : " not != ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 < f2) ? " < " : " not < ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 > f2) ? " > " : " not > ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 <= f2) ? " <= " : " not <= ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 >= f2) ? " >= " : " not >= ") << "f2" << '\n';
+	return 0;
+}
